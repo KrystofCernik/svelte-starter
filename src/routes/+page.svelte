b6709158ce95bdd84736a65284876fca6bbc4fd8
Krystof Cernik <96994380+KrystofCernik@users.noreply.github.com>
2023-05-25 10:12:37 +0200

    test commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import Fa from 'svelte-fa'
 	import { faFlag } from '@fortawesome/free-solid-svg-icons'
 </script>
-<div class="text-white">
-	New app
-	<Fa icon={faFlag} />
+<div class="h-screen w-full bg-white-default flex items-center justify-center">
+	<h1 class="text-[4rem] font-medium">Svelte starter</h1>
+	<!-- <Fa icon={faFlag} /> -->
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'white-default': '#f5f5f5'
+			}
+		},
 	},
 	plugins: [],
 }
